151e4caa73d5da86555abc5da6fbe2dc1dadf2f1
alainburindi <alainburindi62@gmail.com>
2019-03-13 14:17:16 +0200

    allow user to register using api endepoint

---
+++ app.js
@@ -0,0 +1,54 @@
+import express from 'express'
+import bodyParser from 'body-parser';
+
+const app = express()
+
+import userRoutes from './server/routes/user'
+const port = process.env.PORT || 3000
+
+const todo = {
+    "notice" : "this is the V1 api so make sure to put /api/v1/ before any route. eg: api/v1/auth/login",
+     "GET/" : {
+        "/" : "the current result",
+    },
+
+    "POST/" : {
+        "auth/signup" : "register a new user",
+    },
+ }
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.json())
+
+const v1 = "/api/v1";
+
+app.use(`${v1}/auth`, userRoutes)
+
+
+app.get('/',(req, res, next) => {
+    res.status(200).json({
+        status : 200,
+        message : "welcome to the EpicMail Api, below is how to use it",
+        todo
+    })
+})
+app.use((req, res, next) => {
+    const error = new Error('not found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error : {
+            status : error.status,
+            message : error.message
+        }
+    })
+})
+
+
+app.listen(port)
+
+module.exports = app;
+
---
+++ server/middleware/validator.js
@@ -0,0 +1,13 @@
+import Joi from 'joi';
+
+export default class Validator {
+    static schemaSignUp(user) {
+        const userSchema ={
+            name: Joi.string().required(),
+            email : Joi.string().email().required(),
+            password : Joi.string().regex(/^[a-zA-Z0-9]{6,16}$/).min(6).required(),
+            confirmPassword : Joi.string().valid(Joi.ref('password')).required().strict(),
+        }
+        return Joi.validate(user, userSchema);
+    }
+}
