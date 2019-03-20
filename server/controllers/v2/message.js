7e05314e1ad20c383c6ea36e0be6b0491d65dca5
alainburindi <alainburindi62@gmail.com>
2019-03-20 05:13:40 +0200

    allow user to view unread messages

--- server/controllers/v2/message.js
+++ server/controllers/v2/message.js
@@ -81,6 +81,25 @@ export default class MessageController {
         })
     }
 
+    static unread(req, res, next){
+        const {userId} = req.userData
+        const query = "SELECT m.id, m.subject, m.message, m.status, m.parentmessageid, m.created_at, u.email as from FROM Messages as m INNER JOIN Users as u ON m.userid = u.id WHERE receiverid = $1 AND m.status = $2"
+        const values = [userId, 'unread']
+        db(query, values, (err, result) => {
+            if (err)
+            return next(err)
+            if(result.rowCount > 0){
+                res.status(200).json({
+                    data : [result.rows]
+                })
+            }else{
+                res.status(200).json({
+                    data : "no message for you"
+                })
+            }
+        })
+    }
+
     validationError(res, error) {
         res.status(422).json({
            status : 422 ,
