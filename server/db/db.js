5afade3294a86b1cb3630c197c659947ba197fcc
alainburindi <alainburindi62@gmail.com>
2019-03-22 06:17:27 +0200

    test heroku db

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,8 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const pool = new Pool({
-    connectionString : process.env.DATABASE_URL
+    connectionString : process.env.DATABASE_URL,
+    ssl: true
 })
 
 const query = (text, params, callback) => pool.query(text, params, callback)
